b295b438f22ea6d4c21272d999eaa72e76a7303e
quirogak <quirogakill@gmail.com>
2023-03-10 14:54:57 -0500

    Finished testing on reverseString function.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-
 const capitalize = (word) => {
+  const firstLetter = word.slice(0, 1);
+  const upperLetter = firstLetter.toUpperCase();
+  return word.replace(firstLetter, upperLetter);
+};
 
-    const firstLetter = word.slice(0, 1)
-    const upperLetter = firstLetter.toUpperCase()
-    return word.replace(firstLetter, upperLetter)
+const reverseString = (word) => {
+  const reverseWord = [];
 
-}
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverseWord.push(word[i]);
+  }
 
-export default capitalize 
+  return reverseWord.join("");
+};
+
+export { capitalize, reverseString };
--- script.test.js
+++ script.test.js
@@ -1,11 +1,17 @@
-import capitalize from './script'
+import { capitalize, reverseString } from "./script";
 
-console.log(capitalize)
-
-test('the first letter is capitalized', () => {
-    expect(capitalize("h")).toBe("H");
+test("the first letter is capitalized", () => {
+  expect(capitalize("h")).toBe("H");
 });
 
-test('returns the full string capitalized', () => {
-    expect(capitalize("hello")).toBe("Hello");
+test("returns the full string capitalized", () => {
+  expect(capitalize("hello")).toBe("Hello");
+});
+
+test("returns a short string backwards", () => {
+  expect(reverseString("he")).toBe("eh");
+});
+
+test("returns a long, spaced string backwards", () => {
+  expect(reverseString("Hello how are you")).toBe("uoy era woh olleH");
 });
